ffbb901a65d52d3d2a2df09676a973c3c8ebf738
skythomp16 <skythomp16@gmail.com>
2018-07-11 00:07:09 -0500

    Fixed a bug that cuased the program to crash; renamed some variables

--- main.c
+++ main.c
@@ -16,7 +16,7 @@ typedef struct {
 
 //Image structure
 typedef struct {
-    unsigned int x, y;  //Included for rebuilding the image
+    unsigned int width, height;  //Included for rebuilding the image
     PPMPixel *data;
 } PPMImage;
 
@@ -67,7 +67,7 @@ static PPMImage *readPPM(const char *filename)
     ungetc(c, fp);
 
     //read image size information
-    if (fscanf(fp, "%u %u", &img->x, &img->y) != 2) {
+    if (fscanf(fp, "%u %u", &img->width, &img->height) != 2) {
         fprintf(stderr, "Invalid image size (error loading '%s')\n", filename);
         exit(1);
     }
@@ -86,7 +86,7 @@ static PPMImage *readPPM(const char *filename)
 
     while (fgetc(fp) != '\n');
     //memory allocation for pixel data
-    img->data = (PPMPixel*)malloc(img->x * img->y * sizeof(PPMPixel));
+    img->data = (PPMPixel*)malloc(img->width * img->height * sizeof(PPMPixel));
 
     if (!img) {
         fprintf(stderr, "Unable to allocate memory\n");
@@ -94,7 +94,7 @@ static PPMImage *readPPM(const char *filename)
     }
 
     //read pixel data from file
-    if (fread(img->data, 3 * img->x, img->y, fp) != img->y) {
+    if (fread(img->data, 3 * img->width, img->height, fp) != img->height) {
         fprintf(stderr, "Error loading image '%s'\n", filename);
         exit(1);
     }
@@ -122,13 +122,13 @@ void writePPM(const char *filename, PPMImage *img)
     fprintf(fp, "# Created by %s\n", CREATOR);
 
     //image size
-    fprintf(fp, "%d %d\n", img->x, img->y);
+    fprintf(fp, "%d %d\n", img->width, img->height);
 
     // rgb component depth
     fprintf(fp, "%d\n", RGB_COMPONENT_COLOR);
 
     // pixel data
-    fwrite(img->data, 3 * img->x, img->y, fp);
+    fwrite(img->data, 3 * img->width, img->height, fp);
     fclose(fp);
 }
 
@@ -137,22 +137,26 @@ void colorQuantizationPPM(PPMImage *img, int numColors)
     //Filling an array with random numbers for number of colors to be quantized down to
     int numFixedColors = 64;
 
+    //Random number generator (for selecting random centers)
+    srand(time(0));
+
     //Make an array of clusters to coorespond with the array of centers
-    PPMCluster *clusters = (PPMCluster *) malloc(numFixedColors * sizeof(PPMCluster));
+    PPMCluster* clusters = malloc(numFixedColors * sizeof(*clusters));
 
     //Variable to indicate the number of pixels in the image
-    int numPixels = (img->x * img->y);
-
+    int numPixels = (img->width * img->height);
 
     for (int i = 0; i < numFixedColors; i++)
     {
         PPMPixel myTemp = img->data[rand() % (numPixels) + 1];
         clusters[i].center = myTemp;
+        clusters[i].size = 1;
     }
+
 
     //Now time for data clustering using k-means
     //First, Some variables
-    int terminate = numPixels; //terminates after going through every pixel in the image
+    int terminate = numFixedColors; //terminates after iterating however many times the colors need to be quantized
     int randPixNum;
     int index = 0;
     PPMPixel closest;
@@ -164,7 +168,7 @@ void colorQuantizationPPM(PPMImage *img, int numColors)
     int nearest;
 
     //Terminate when all pixels in the center array have been gone through
-    for ( index = 0; index < terminate; index++ )
+    for (index = 0; index < numFixedColors; index++ )
      {
         //Now, select a random pixel from the pixel array 
         randPixNum = rand();
@@ -179,43 +183,45 @@ void colorQuantizationPPM(PPMImage *img, int numColors)
             diffG = (randPix.green - temp.center.green) * (randPix.green - temp.center.green);
             diffR = (randPix.red - temp.center.red) * (randPix.red - temp.center.red);
             
-	    double tempTotalRGB = diffR + diffG + diffB;
+	        double tempTotalRGB = diffR + diffG + diffB;
 
             if (tempTotalRGB < totalRGB)
             {
                 totalRGB = tempTotalRGB;
 		        nearest = i;
             }
-        }
+        
 
         //Update the center in the centers array ci = (Ni ci + xr)/(Ni + 1)
-	// NEW
-	// old_size = fetch the current size of cluster i
-	// new_size = old_size + 1;
-	// center[i].red = ( old_size * center[i].red + randPix.red ) / ( double ) new_size;
-	// center[i].green = ( old_size * center[i].green + randPix.green ) / ( double ) new_size;
-	// center[i].blue = ( old_size * center[i].blue + randPix.blue ) / ( double ) new_size;
-	// update size of cluster i as new_size
+        /* Comments from Dr. Celebi
+            // NEW
+            // old_size = fetch the current size of cluster i
+            // new_size = old_size + 1;
+            // center[i].red = ( old_size * center[i].red + randPix.red ) / ( double ) new_size;
+            // center[i].green = ( old_size * center[i].green + randPix.green ) / ( double ) new_size;
+            // center[i].blue = ( old_size * center[i].blue + randPix.blue ) / ( double ) new_size;
+            // update size of cluster i as new_size
+        */
+            printf("%d\n", clusters[index].size);
 
-    int old_size = clusters[index].size;
-    int new_size = old_size + 1;
-    clusters[index].center.red = ( old_size * clusters[index].center.red + randPix.red ) / ( double ) new_size;
-    clusters[index].center.green = ( old_size * clusters[index].center.green + randPix.green ) / ( double ) new_size;
-    clusters[index].center.blue = ( old_size * clusters[index].center.blue + randPix.blue ) / ( double ) new_size;
-    clusters[index].size = new_size;
+            int old_size = clusters[index].size;
+            int new_size = old_size + 1;
+
+            clusters[index].center.red = ( old_size * clusters[index].center.red + randPix.red ) / (double) new_size;
+            clusters[index].center.green = ( old_size * clusters[index].center.green + randPix.green ) / (double) new_size;
+            clusters[index].center.blue = ( old_size * clusters[index].center.blue + randPix.blue ) / (double) new_size;
+            
+            clusters[nearest].size = new_size;
+        }
+    
     } 
-
 }
 
 //Main function
 int main() {
-
     //Create a new image object and read the image in
     PPMImage *image;
     image = readPPM("sample.ppm");
-
-    //Random number generator (for selecting random centers)
-    srand(time(0));
 
     //Run color quantization on the image
     colorQuantizationPPM(image, 64);
